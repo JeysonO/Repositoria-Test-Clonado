3aae60e7fb1576d1da8634031df12b314e3791c9
Luis Soto <lsoto@complexless.com>
2024-08-06 23:56:01 -0500

    Mejoras para despiegue de consulta cuo

--- src/main/java/pe/com/amsac/tramite/pide/soap/cuo/request/GetCUOEntidad.java
+++ src/main/java/pe/com/amsac/tramite/pide/soap/cuo/request/GetCUOEntidad.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por Eclipse Implementation of JAXB v2.3.7 
 // Visite https://eclipse-ee4j.github.io/jaxb-ri 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2024.08.06 a las 11:00:38 PM PET 
+// Generado el: 2024.08.06 a las 11:54:10 PM PET 
 //
 
 
